b6b842b7844be0fe8c803747f9404ce120cdd12f
milantondutta <milantondutta@gmail.com>
2025-06-01 20:14:26 +0530

    code update

--- README.md
+++ README.md
@@ -1,37 +1,56 @@
 
-#Automated end-to-end test of a ToDo app using Playwright (JavaScript)
+# Automated end-to-end test of a ToDo app using Playwright (JavaScript)
+
+Demo app used for the Test
+    https://demo.playwright.dev/todomvc
 
 
-Setup Instruction
+
+Setup Instructions
+
+Step 1- Clone the project
+    git clone https://github.com/milantondutta/Assignment
+
+Step 2- Navitage to the project directory
+    cd Assignment
+
+Step 3- Install all project dependencies
+    npm install
 
 
 
-Instructions to execute test
+Instructions to execute Test
 
--To execute test in headless mode -> npx playwright test
--To execute test in headed mode -> npx playwright test --headed
--To execute test in UI mode -> npx playwright test --ui
--To get test report -> npx playwright show-report
-
+    To execute test in headless mode  ->     npx playwright test
+    To execute test in headed mode    ->     npx playwright test --headed
+    To execute test in UI mode        ->     npx playwright test --ui
+    To get test report                ->     npx playwright show-report
 
 
 
 Test Scenarios
 
--Add a Todo
--Mark a Todo as Completed
--Delete a Todo
--Edit a Todo
--Filter Todos(All, Active, Completed)
--Negative Test(Empty todo)
--Screenshot on failure
-
+    Add a Todo
+    Mark a Todo as Completed
+    Delete a Todo
+    Edit a Todo
+    Filter Todos(All, Active, Completed)
+    Negative Test(Empty todo)
+    Screenshot on failure
 
 
 
 Test Design Decisions Notes
 
--All tests are independent and can be run separately
--All tests includes basic error handling and assertions (Playwright expect)
--Implemented Page Object Model for maintainability
--
+Independent Tests: 
+    All tests are isolated and can be executed individually which improves reliability.
+
+Assertions and Error Handling: 
+    All tests includes validation using Playwright’s built-in assertion library 'expect' to improve test 
+    robustness and provide easier debugging.
+
+Page Object Model (POM): 
+    Tests are structured using POM for better readability and maintainability.
+
+Comments for Clarity: 
+    Key steps in the test scripts are clearly commented to improve readability and understanding.
--- tests/ToDo.spec.js
+++ tests/ToDo.spec.js
@@ -5,16 +5,8 @@ import {CreateTodo} from '../pageObjects/CreateTodo'
 
 
 // Navigating to the application
-
-
 test.beforeEach(async({page}) => {
-    try{
     await page.goto('https://demo.playwright.dev/todomvc')
-    }
-    catch(error){
-        console.error(error)
-        throw new Error("Something went wrong!")
-    }
 })
 
 
@@ -22,71 +14,47 @@ test.beforeEach(async({page}) => {
 // Adding a new ToDo
 test('Add a Todo', async ({page}) => {
 
-    try{
-        // Adding a New ToDo
-        const newTodo = new CreateTodo(page)
-        await newTodo.todo()
+    // Adding a New ToDo
+    const newTodo = new CreateTodo(page)
+    await newTodo.todo()
 
-        // This is to be printed if the task is successful
-        console.log('Success')
-    }
-        // This is to be printed if the task fails
-        catch (error) {
-            console.log('Something went wrong')
-    }
-        // Verifying if the added ToDo appears on the list
-        await expect(page.locator('[data-testid="todo-title"]')).toHaveText('Buy fruits')
-        await expect(page.locator('[data-testid="todo-title"]')).toHaveCount(1)
+    // Verifying if the added ToDo appears on the list
+    await expect(page.locator('[data-testid="todo-title"]')).toHaveText('Buy fruits')
+    await expect(page.locator('[data-testid="todo-title"]')).toHaveCount(1)
 })
-
+       
 
 
 // Marking a Todo as Completed
 test('Mark a ToDo as completed', async ({page}) => {
 
-    try{
-        // Adding a New ToDo
-        const newTodo = new CreateTodo(page)
-        await newTodo.todo()
+    // Adding a New ToDo
+    const newTodo = new CreateTodo(page)
+    await newTodo.todo()
 
-        // Navigating to toggle button and applying a check on it
-        await page.locator('[class="toggle"]').check()
+    // Navigating to toggle button and applying a check on it
+    await page.locator('[class="toggle"]').click()
 
-        // This is to be printed if the task is successful
-            console.log('Success')
-    }
-        // This is to be printed if the task fails
-        catch (error) {
-            console.log('Something went wrong')
-    }
-        // Verifying if the checked ToDo is marked as completed
-        await expect(page.locator('[data-testid="todo-item"]')).toHaveClass(/completed/)
-        await expect(page.locator('[data-testid="todo-item"]')).toHaveCount(1)
+    // Verifying if the checked ToDo is marked as completed
+    await expect(page.locator('[data-testid="todo-item"]')).toHaveClass(/completed/)
+    await expect(page.locator('[data-testid="todo-item"]')).toHaveCount(1)
 })
 
-
+   
 
 // Deleting a ToDo
 test('Delete a ToDo', async ({page}) => {
 
-    try{
-        // Adding a New ToDo
-        const newTodo = new CreateTodo(page)
-        await newTodo.todo()
+    // Adding a New ToDo
+    const newTodo = new CreateTodo(page)
+    await newTodo.todo()
 
-        // Hovering over the active ToDo and making a click on the delete button 
-        await page.locator('[data-testid="todo-title"]').hover()
-        await page.locator('[aria-label="Delete"]').click()
+    // Hovering over the active ToDo and making a click on the delete button 
+    await page.locator('[data-testid="todo-title"]').hover()
+    await page.locator('[aria-label="Delete"]').click()
 
-        // This is to be printed if the task is successful
-        console.log('Success!')
-    }
-        // This is to be printed if the task fails
-        catch (error) {
-            console.log('Something went wrong')
-    }
-        // Verifying if the deleted ToDo is gone
-        await expect(page.locator('[data-testid="todo-title"]')).toHaveCount(0)
+    // Verifying if the deleted ToDo is gone
+    await expect(page.locator('[data-testid="todo-title"]')).toHaveCount(0)
 })
 
 
@@ -94,27 +62,19 @@ test('Delete a ToDo', async ({page}) => {
 // Editing a ToDo
 test('Double Click to edit a ToDo', async ({page}) => {
     
-    try{
-        // Adding a New ToDo
-        const newTodo = new CreateTodo(page)
-        await newTodo.todo()
+    // Adding a New ToDo
+    const newTodo = new CreateTodo(page)
+    await newTodo.todo()
 
-        // Double clicking on the active ToDo which is to be edited
-        await page.locator('[data-testid ="todo-title"]').dblclick()
+    // Double clicking on the active ToDo which is to be edited
+    await page.locator('[data-testid ="todo-title"]').dblclick()
     
-        // Editing the ToDo
-        await page.locator('[class="edit"]').fill('Water the plants')
-        await page.locator('[class="edit"]').press('Enter')
+    // Editing the ToDo
+    await page.locator('[class="edit"]').fill('Go for a jog')
+    await page.locator('[class="edit"]').press('Enter')
 
-        // This is to be printed if the task is successful
-        console.log('Success!')
-    }
-        // This is to be printed if the task fails
-        catch(error){
-            console.log('Something went wrong!')
-    }
-        // Verifying if the new ToDo is updated 
-        await expect(page.locator('[data-testid="todo-title"]')).toHaveText('Water the plants')
+    // Verifying if the new ToDo is updated 
+    await expect(page.locator('[data-testid="todo-title"]')).toHaveText('Go for a jog')
 })
 
 
@@ -122,75 +82,53 @@ test('Double Click to edit a ToDo', async ({page}) => {
 // Filtering a Todo
 test('Filter - All', async ({page}) => {
 
-    try{
-        // Adding a New ToDo
-        const newTodo = new CreateTodo(page)
-        await newTodo.todo()
+    // Adding a New ToDo
+    const newTodo = new CreateTodo(page)
+    await newTodo.todo()
 
-        // Making a click on the filter "All" icon
-        await page.locator(':text-is("All")').click()
+    // Making a click on the filter "All" icon
+    await page.locator(':text-is("All")').click()
 
-        // This is to be printed if the task is successful
-        console.log('Success!')
-    }
-        // This is to be printed if the task fails
-        catch(error){
-            console.log('Something went wrong!')
-    }
-        // Verifying if the filter "All" icon is selected and the Todo appears on the list
-        await expect(page.locator(':text-is("All")')).toHaveClass(/selected/)
-        await expect(page.locator('[data-testid="todo-title"]')).toHaveText('Buy fruits')
+   // Verifying if the filter "All" icon is selected and the Todo appears on the list
+    await expect(page.locator(':text-is("All")')).toHaveClass(/selected/)
+    await expect(page.locator('[data-testid="todo-title"]')).toHaveText('Buy fruits')
 })
 
 
 
 test('Filter - Active', async ({page}) => {
 
-    try{
-        // Adding a New ToDo
-        const newTodo = new CreateTodo(page)
-        await newTodo.todo()
+    // Adding a New ToDo
+    const newTodo = new CreateTodo(page)
+    await newTodo.todo()
 
-        // Making a click on the filter "Active" icon
-        await page.locator(':text-is("Active")').click()
+    // Making a click on the filter "Active" icon
+    await page.locator(':text-is("Active")').click()
 
-        // This is to be printed if the task is successful
-        console.log('Success!')
-    }
-        // This is to be printed if the task fails
-        catch(error){
-            console.log('Something went wrong!')
-    }
-        // Verifying if the filter "Active" icon is clicked and the active Todo appears on the list
-        await expect(page.locator(':text-is("Active")')).toHaveClass(/selected/)
-        await expect(page.locator('[data-testid="todo-title"]')).toHaveText('Buy fruits')
+    
+    // Verifying if the filter "Active" icon is clicked and the active Todo appears on the list
+    await expect(page.locator(':text-is("Active")')).toHaveClass(/selected/)
+    await expect(page.locator('[data-testid="todo-title"]')).toHaveText('Buy fruits')
 })
 
 
 
 test('Filter - Completed', async ({page}) => {
 
-    try{
-        // Adding a New ToDo
-        const newTodo = new CreateTodo(page)
-        await newTodo.todo()
+    // Adding a New ToDo
+    const newTodo = new CreateTodo(page)
+    await newTodo.todo()
 
-        // Navigating to toggle button and making a check on it
-        await page.locator('[class="toggle"]').check()
+    // Navigating to toggle button and making a check on it
+    await page.locator('[class="toggle"]').check()
 
-        // Making a click on the filter "Completed" icon
-        await page.locator(':text-is("Completed")').click()
+    // Making a click on the filter "Completed" icon
+    await page.locator(':text-is("Completed")').click()
 
-        // This is to be printed if the task is successful
-        console.log('Success!')
-    }   
-        // This is to be printed if the task fails
-        catch(error){
-            console.log('Something went wrong!')
-    }   
-        // Verifying if the filter "Completed" icon is clicked and the completed Todo appears on the list
-        await expect(page.locator(':text-is("Completed")')).toHaveClass(/selected/)
-        await expect(page.locator('[data-testid="todo-title"]')).toHaveCount(1)
+ 
+    // Verifying if the filter "Completed" icon is clicked and the completed Todo appears on the list
+    await expect(page.locator(':text-is("Completed")')).toHaveClass(/selected/)
+    await expect(page.locator('[data-testid="todo-title"]')).toHaveCount(1)
 })
 
 
@@ -198,21 +136,16 @@ test('Filter - Completed', async ({page}) => {
 // Negative test
 test('Adding an empty task', async({page}) => {
 
-    try{
-        // Adding a New ToDo
-        await page.locator('//input[@class="new-todo"]').click()
-        await page.locator('//input[@class="new-todo"]').fill('')
-        await page.locator('//input[@class="new-todo"]').press('Enter')
+    // Adding a New ToDo
+    await page.locator('//input[@class="new-todo"]').click()
+    await page.locator('//input[@class="new-todo"]').fill('')
+    await page.locator('//input[@class="new-todo"]').press('Enter')
         
-        // This is to be printed if the task is successful
-        console.log('Success!')
-    }
-        // This is to be printed if the task fails
-        catch(error){
-            console.log('Something went wrong!')
-    }
-        // Verifying no todo is added
-        await expect(page.locator('[data-testid="todo-title"]')).toHaveCount(0)
+    // This is to be printed if the task is successful
+    console.log('Success!')
+    
+    // Verifying no todo is added
+    await expect(page.locator('[data-testid="todo-title"]')).toHaveCount(0)
 })
 
 
@@ -220,24 +153,19 @@ test('Adding an empty task', async({page}) => {
 // Screenshot on failure
 test('Screenshot of a failed test', async ({page}) => {
 
-    try{
-        // Add a ToDo item
-        await page.locator('//input[@class="new-todo"]').click();
-        await page.locator('//input[@class="new-todo"]').fill('Clean the room');
-        await page.locator('//input[@class="new-todo"]').press('Enter');
+try{
+    // Add a ToDo item
+    await page.locator('//input[@class="new-todo"]').click();
+    await page.locator('//input[@class="new-todo"]').fill('Clean the room');
+    await page.locator('//input[@class="new-todo"]').press('Enter');
 
-        // This is to be printed if the task is successful
-        console.log('Success')
-        
-        // Intentionally failing the assertion
-        await expect(page.locator('[data-testid="todo-title"]')).toHaveText('Buy fruits')
-    }
-        // This is to be printed if the task fails
-        catch (error) {
-            console.log('Something went wrong')
-    
-        // Screenshot of the page
-        await page.screenshot({ path: 'screenshots/full-page.png', fullPage: true })
-    }
+   
+    // Intentionally failing the assertion
+    await expect(page.locator('[data-testid="todo-title"]')).toHaveText('Water the plants')
+}   
+catch{
+    // Screenshot of the page
+    await page.screenshot({ path: 'screenshots/full-page.png', fullPage: true })
+}    
 })
 
